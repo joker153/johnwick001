eb451e35cc531abe761439c1b434bbd57ce94aa3
joker153 <77005994+joker153@users.noreply.github.com>
2021-10-30 00:23:47 -0700

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,4 +1,4 @@
-johnwick001botclass script(object):
+class script(object):
     START_TXT = """𝙷𝙴𝙻𝙾 {},
 𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href='https://t.me/johnwick001bot'>JOHN WICK</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍"""
     HELP_TXT = """𝙷𝙴𝚈 {}
